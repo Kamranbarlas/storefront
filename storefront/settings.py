913f7d8908b0baf8742b16149ea2cb8934dbf855
Kamran Barlas <Kamran_Barlas@hotmail.com>
2024-08-04 18:01:35 +0500

    models Updated to Admin Site Finish

--- storefront/settings.py
+++ storefront/settings.py
@@ -88,8 +88,12 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'storefront',
+        'USER': 'root',
+        'PASSWORD': 'mysql@123',
+        'HOST': 'localhost',  # Or '127.0.0.1'
+        'PORT': '3306',       # Default MySQL port
     }
 }
 
