4db362b33814bfa9fe43456bcf9c88a0e2abf5d3
Kamran Barlas <Kamran_Barlas@hotmail.com>
2024-08-12 15:40:48 +0500

    practising with Serializers

--- store/urls.py
+++ store/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     path('products/', views.product_list),
     path('products/<int:id>/', views.product_detail),
+    path('collections/<int:pk>/', views.collection_detail, name='collection_detail'),
 ]
--- store/views.py
+++ store/views.py
@@ -3,14 +3,18 @@ from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from . models import Product
-from . serializers import ProductSerializer
+from . models import Product, Collection
+from . serializers import ProductSerializer, CollectionSerializer
 # Create your views here.
 
 @api_view()
 def product_list(request):
-    queryset = Product.objects.all()
-    serializer = ProductSerializer(queryset,many=True)
+    queryset = Product.objects.select_related('collection').all()
+    serializer = ProductSerializer(
+        queryset,
+        many=True, 
+        context={'request':request}
+        )
     return Response(serializer.data)
 
 @api_view()
@@ -18,3 +22,7 @@ def product_detail(request,id):
     product = get_object_or_404(Product, pk=id)
     serializer = ProductSerializer(product)
     return Response(serializer.data)
+
+@api_view()
+def collection_detail(request,pk):
+    return Response('ok')
