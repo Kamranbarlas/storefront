761249e2c115dcdb6b347ad6da1b248d98251b9e
Kamran Barlas <Kamran_Barlas@hotmail.com>
2024-08-12 16:08:11 +0500

    ModelSerializer practice

--- store/serializers.py
+++ store/serializers.py
@@ -2,20 +2,25 @@ from decimal import Decimal
 from rest_framework import serializers
 from . models import Product, Collection
 
-class CollectionSerializer(serializers.Serializer):
+class CollectionSerializer(serializers.ModelSerializer):
+    model = Collection
+    fields = ['id','title']
     id  = serializers.IntegerField()
     title = serializers.CharField(max_length=255)
 
 
-class ProductSerializer(serializers.Serializer):
-    id  = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    price = serializers.DecimalField(max_digits=6, decimal_places=2,source='unit_price')
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'unit_price', 'price_with_tax','collection']
+    # id  = serializers.IntegerField()
+    # title = serializers.CharField(max_length=255)
+    # price = serializers.DecimalField(max_digits=6, decimal_places=2,source='unit_price')
     price_with_tax = serializers.SerializerMethodField(method_name="calculate_with_tax")
-    collection = serializers.HyperlinkedRelatedField(
-        queryset = Collection.objects.first(),
-        view_name = 'collection_detail'
-    )
+    # collection = serializers.HyperlinkedRelatedField(
+    #     queryset = Collection.objects.first(),
+    #     view_name = 'collection_detail'
+    # )
 
     def calculate_with_tax(self, product: Product):
         return product.unit_price * Decimal(1.1) 
