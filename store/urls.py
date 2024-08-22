c632592a4ae6a517bbc1749c9326f15cde4c5ef8
Kamran Barlas <Kamran_Barlas@hotmail.com>
2024-08-23 04:55:16 +0500

    implementing API View in views of store app for Product Detail

--- store/urls.py
+++ store/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('products/', views.ProductList.as_view()),
-    path('products/<int:id>/', views.product_detail),
+    path('products/<int:id>/', views.ProductDetail.as_view()),
     path('collections/<int:pk>/', views.collection_detail, name='collection_detail'),
 ]
--- store/views.py
+++ store/views.py
@@ -25,6 +25,26 @@ class ProductList(APIView):
         print(serializer.validated_data)
         return Response(serializer.data, status=status.HTTP_201_CREATED)        
 
+class ProductDetail(APIView):  
+    def get(self, request, id):
+        product = get_object_or_404(Product, pk=id)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    
+    def put(self, request, id):
+        product = get_object_or_404(Product, pk=id)
+        serializer = ProductSerializer(product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    
+    def delete(self, request, id):
+        product = get_object_or_404(Product, pk=id)
+        if product.orderitems.count() > 0:
+            return Response({'error': 'Product cannot be deleted because it is associated with an order item.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 @api_view(['GET', 'PUT', 'DELETE'])
 def product_detail(request, id):
     product = get_object_or_404(Product, pk=id)
