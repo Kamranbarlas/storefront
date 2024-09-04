f3c0b0adc10460015408ec472a774960d58e1a9e
Kamran Barlas <Kamran_Barlas@hotmail.com>
2024-09-05 03:04:15 +0500

    Viewset implementing in store app functionality

--- store/views.py
+++ store/views.py
@@ -7,7 +7,7 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.generics import ListAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.response import Response
 from rest_framework import status
-from . models import Product, Collection
+from . models import Product, Collection, OrderItem
 from . serializers import ProductSerializer, CollectionSerializer
 # Create your views here.
 
@@ -17,13 +17,12 @@ class ProductViewSet(ModelViewSet):
 
     def get_serializer_context(self):
         return {'requset':self.request}
-    
-    def delete(self, request, pk):
-        product = get_object_or_404(Product, pk=pk)
-        if product.orderitems.count() > 0:
+
+    def destroy(self, request, *args, **kwargs):
+        if OrderItem.objects.filter(product_id=kwargs['pk']).count() > 0:
             return Response({'error': 'Product cannot be deleted because it is associated with an order item.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        product.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return super().destroy(request, *args, **kwargs)
+    
 
 
    
