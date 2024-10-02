76ecf43caea755c60a0a5bd86588abdbc39d93f7
Kamran Barlas <Kamran_Barlas@hotmail.com>
2024-10-02 12:53:39 +0500

    getting current user profile

--- store/serializers.py
+++ store/serializers.py
@@ -102,7 +102,7 @@ class UpdateCartItemSerializer(serializers.ModelSerializer):
         fields = ['quantity']
 
 class CustomerSerialier(serializers.ModelSerializer):
-    user_id = serializers.IntegerField()
+    user_id = serializers.IntegerField(read_only=True)
     class Meta:
         model = Customer
         fields = ['id','user_id','phone' ,'birth_date','membership']
